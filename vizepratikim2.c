e452e810ad98ccbf0a2ff677664f3ae2b73c0f51
Murat Can BİLGİN <muratcanbilgin25@gmail.com>
2025-11-19 01:12:37 +0300

    Some mistakes and lessons

---
+++ vizepratikim2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//This Code Busted. Made this when i didnt know how switch-case worked. Chars dont work inside switch parameter control, only integer like number based
+//variables.
+
+int main()
+{
+    char isim[50];
+
+    printf("birsey yaz");
+    scanf("%s", &isim);
+
+    switch(isim)
+    {
+        case "Murat":
+            printf("Bu benim ismim");
+            break;
+
+        default:
+
+        printf("Bu ne?");
+
+
+    }
+
+    return 0;
+}
