3ef0c6d9bd4926b674441beabe23160b041ad0f2
Murat Can BİLGİN <muratcanbilgin25@gmail.com>
2025-12-05 13:59:41 +0300

    Odev2_Soru2.c bitti

---
+++ odev2_soru2.c
@@ -0,0 +1,51 @@
+/*asal sayi kontrolu
+kullanicidan bir pozitif tam sayi girmesini isteyin. girilen sayinin asal olup olmadigini kontrol eden bir C programi yazin.
+*/
+#include <stdio.h>
+
+int main() 
+{
+
+    // Kullanicidan alinan sayinin bolunebilme kurallarini kontrol etmek icin for dongusu kullandik
+    // Dongu 2'den baslar ve girilen sayiya kadar bolunebilmeyi kontrol eder
+    // Eger bolunebilen bir sayi bulur ise sayi bolunebilme kontrol sayisini degistirir
+    // Program dongu sonunda kontrol sayisina bagli olarak asal olup olmadigini yazar
+
+    int bolunebilme_kontrol_sayisi;
+
+    int asal_sorgulama_degiskeni = 0;
+
+    int kullanicidan_alinan_sayi;
+
+    printf("Bir sayi giriniz: ");
+    scanf("%d", &kullanicidan_alinan_sayi);
+
+
+for(bolunebilme_kontrol_sayisi = 2; 2 < kullanicidan_alinan_sayi ;bolunebilme_kontrol_sayisi++)
+{
+
+  if(kullanicidan_alinan_sayi % bolunebilme_kontrol_sayisi == 0)
+  {
+
+    int asal_sorgulama_degiskeni = 1;
+    break;
+
+  }
+
+}
+
+if(asal_sorgulama_degiskeni == 1)
+{
+
+    printf("sayi asaldir");
+    
+}else
+{
+
+   printf("sayi asal degildir");
+
+}
+
+return 0;
+
+}
