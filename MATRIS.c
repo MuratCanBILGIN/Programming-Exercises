ce491ab86ea9e25650bf3130ba210702cea9a167
Murat Can BİLGİN <muratcanbilgin25@gmail.com>
2026-02-20 21:00:18 +0300

    Work 19.00-21.00

---
+++ MATRIS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef float MATRIS[10][20];
+
+int main()
+{
+
+    MATRIS y;
+
+    y[0][0] = 1;
+
+    printf("%f",y[0][0]);
+
+
+
+
+    return 0;
+}
---
+++ TABLO.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef char *TABLO[10];
+
+int main()
+{
+
+    TABLO x;
+
+    x[0] = "Can";
+
+    printf("%s",x[0]);
+
+
+
+
+    return 0;
+}
